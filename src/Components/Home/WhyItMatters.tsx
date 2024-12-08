d63aed1365601dd433624b64334a579230ef2a34
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-12-08 17:13:24 -0500

    finished WhyItMatters

--- src/Components/Home/WhyItMatters.tsx
+++ src/Components/Home/WhyItMatters.tsx
@@ -1,7 +1,18 @@
 import { useTranslation } from "react-i18next";
+import AOS from "aos";
+import { FaStar } from "react-icons/fa";
+import { IoShieldCheckmarkSharp } from "react-icons/io5";
+import { FaSmile } from "react-icons/fa";
+import { FaChartLine } from "react-icons/fa";
+import { FaCoins } from "react-icons/fa";
+import { useEffect } from "react";
 
 function WhyItMatters() {
     const { t } = useTranslation("whyItMatters");
+
+    useEffect(() => {
+      AOS.init({duration: 1000})
+    }, [])
   return (
     <section className="bg-slate-200 py-20">
       <div className="container">
@@ -11,42 +22,91 @@ function WhyItMatters() {
         <p className="text-2xl text-gray-700 font-semibold">
           {t("whyItMatters.para")}
         </p>
-        <p className="text-2xl font-bold text-blue-900 mt-6">
+        <p className="text-2xl font-bold text-blue-900 mt-6 mb-5">
           {t("whyItMatters.titleTwo")}
         </p>
-        <ul>
-          <li className="text-xl text-gray-700 mt-3">
-            <span className="font-bold">
+        <article className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div
+            className="flex flex-col items-center p-7 bg-amber-50 shadow-lg rounded-lg"
+            data-aos="flip-right"
+            data-aos-delay="100"
+          >
+            <FaStar className="text-5xl text-amber-400" />
+            <h3 className="text-2xl font-bold text-gray-700 mt-3 mb-5">
               {t("whyItMatters.firstImpressions")}
-            </span>
-            {t("whyItMatters.liElOne")}
-          </li>
-          <li className="text-xl text-gray-700 mt-3">
-            <span className="font-bold">
+            </h3>
+            <p className="text-xl text-gray-700">{t("whyItMatters.liElOne")}</p>
+          </div>
+
+          <div
+            className="flex flex-col items-center p-7 bg-blue-50 shadow-lg rounded-lg"
+            data-aos="flip-right"
+            data-aos-delay="250"
+          >
+            <IoShieldCheckmarkSharp className="text-5xl text-blue-500" />
+            <h3 className="text-2xl font-bold text-gray-700 mt-3 mb-5">
               {t("whyItMatters.buildsCredibility")}
-            </span>
-            {t("whyItMatters.liElTwo")}
-          </li>
-          <li className="text-xl text-gray-700 mt-3">
-            <span className="font-bold">
+            </h3>
+            <p className="text-xl text-gray-700">{t("whyItMatters.liElTwo")}</p>
+          </div>
+
+          <div
+            className="flex flex-col items-center p-7 bg-yellow-50 shadow-lg rounded-lg"
+            data-aos="flip-right"
+            data-aos-delay="400"
+          >
+            <FaSmile className="text-5xl text-yellow-500" />
+            <h3 className="text-2xl font-bold text-gray-700 mt-3 mb-5">
               {t("whyItMatters.customerExperience")}
-            </span>
-            {t("whyItMatters.liElThree")}
-          </li>
-          <li className="text-xl text-gray-700 mt-3">
-            <span className="font-bold">
+            </h3>
+            <p className="text-xl text-gray-700">
+              {t("whyItMatters.liElThree")}
+            </p>
+          </div>
+
+          <div
+            className="flex flex-col items-center p-7 bg-green-50 shadow-lg rounded-lg"
+            data-aos="flip-right"
+            data-aos-delay="550"
+          >
+            <FaChartLine className="text-5xl text-green-500" />
+            <h3 className="text-2xl font-bold text-gray-700 mt-3 mb-5">
               {t("whyItMatters.searchEngineVisibility")}
-            </span>
-            {t("whyItMatters.liElFour")}
-          </li>
-          <li className="text-xl text-gray-700 mt-3">
-            <span className="font-bold">
+            </h3>
+            <p className="text-xl text-gray-700">
+              {t("whyItMatters.liElFour")}
+            </p>
+          </div>
+
+          <div
+            className="flex flex-col items-center p-7 bg-gray-50 shadow-lg rounded-lg"
+            data-aos="flip-right"
+            data-aos-delay="700"
+          >
+            <FaCoins className="text-5xl text-amber-400" />
+            <h3 className="text-2xl font-bold text-gray-700 mt-3 mb-5">
               {t("whyItMatters.increasesConversions")}
-            </span>
-            {t("whyItMatters.liElFive")}
-          </li>
-        </ul>
-        <p className="mt-5 text-2xl font-bold text-cyan-700">{t("whyItMatters.lastPara")}</p>
+            </h3>
+            <p className="text-xl text-gray-700">
+              {t("whyItMatters.liElFive")}
+            </p>
+          </div>
+
+          <div
+            className="flex flex-col items-center p-7 bg-gray-50 shadow-lg rounded-lg"
+            data-aos="flip-right"
+            data-aos-delay="850"
+          >
+            <FaCoins className="text-5xl text-amber-400" />
+            <h3 className="text-2xl font-bold text-gray-700 mt-3 mb-5">
+              {t("whyItMatters.responsiveDesign")}
+            </h3>
+            <p className="text-xl text-gray-700">{t("whyItMatters.liElSix")}</p>
+          </div>
+        </article>
+        <p className="mt-16 text-2xl font-bold text-cyan-700">
+          {t("whyItMatters.lastPara")}
+        </p>
       </div>
     </section>
   );
