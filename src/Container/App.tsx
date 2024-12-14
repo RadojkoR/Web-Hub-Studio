76e7bcb3528b1f75a2803e6e52c732bfa1a86984
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-12-14 00:08:37 -0500

    added useEffect for lang change

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -3,17 +3,21 @@ import { Route, Routes } from 'react-router-dom'
 import '../App.css';
 import '../assets/styles/main.scss';
 import { Contact, Home, Projects, SeoOptimization, Services, WebDesign, WebDevelopment } from '../Pages'
-// import { Footer, Nav } from '../Components/Layout';
 import Layout from '../Components/Layout/Layout';
-
+import { useTranslation } from 'react-i18next';
+import { useEffect } from 'react';
 
 function App() {
+const { i18n } = useTranslation();
 
+ useEffect(() => {
+   const language = i18n.language || "en";
+   document.documentElement.setAttribute("lang", language);
+ }, [i18n.language]);
 
   return (
     <>
-      {/* <Nav /> */}
-      {/* <section> */}
+      
         <Routes>
           <Route  element={<Layout />}>
           <Route path="/" element={<Home />} />
@@ -27,9 +31,6 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           </Route>
         </Routes>
-      {/* </section> */}
-
-      {/* <Footer /> */}
     </>
   );
 }
