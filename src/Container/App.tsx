b8d62e6452fda60bcd39b692d829f016f9dbbdc0
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-12-14 00:25:23 -0500

    ....

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -5,21 +5,34 @@ import '../assets/styles/main.scss';
 import { Contact, Home, Projects, SeoOptimization, Services, WebDesign, WebDevelopment } from '../Pages'
 import Layout from '../Components/Layout/Layout';
 import { useTranslation } from 'react-i18next';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
+import { Helmet } from 'react-helmet';
 
 function App() {
 const { i18n } = useTranslation();
 
- useEffect(() => {
-   const language = i18n.language || "en";
-   document.documentElement.setAttribute("lang", language);
- }, [i18n.language]);
+//  useEffect(() => {
+//    const language = i18n.language || "en";
+//    document.documentElement.setAttribute("lang", language);
+//  }, [i18n.language]);
 
   return (
     <>
-      
-        <Routes>
-          <Route  element={<Layout />}>
+      <Helmet>
+        <html lang={i18n.language} />
+
+        <meta
+          name="description"
+          content="Web Hub Studio - Professional Web Development Services"
+        />
+        <meta name="robots" content="index, follow" />
+
+        <link rel="alternate" hrefLang="en" href="https://webhubstudio.netlify.app/en" />
+        <link rel="alternate" hrefLang="sr" href="http://https://webhubstudio.netlify.app/sr" />
+      </Helmet>
+
+      <Routes>
+        <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="services">
             <Route index element={<Services />} />
@@ -29,8 +42,8 @@ const { i18n } = useTranslation();
           </Route>
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
-          </Route>
-        </Routes>
+        </Route>
+      </Routes>
     </>
   );
 }
